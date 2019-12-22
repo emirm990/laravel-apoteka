fce0ab510d87747913045131664c60349a8254b4
Emir Mekić <emir.mekic@live.com>
2019-12-22 22:30:15 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@
 
 Route::get('/','ItemsController@index');
 Route::post('/items/search', 'ItemsController@search');
+Route::get('/items/new', 'ItemsController@create')->middleware('isAdmin');
+Route::post('/items/new', 'ItemsController@store')->middleware('isAdmin');
 Route::get('/items/{item_id}/edit' , 'ItemsController@edit')->middleware('isAdmin');
 Route::get('/items/{item_id}' , 'ItemsController@show')->middleware('isAdmin');
 Route::patch('/items/{item_id}/edit', 'ItemsController@update')->middleware('isAdmin');
+
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
