ca18b4b53ea96dbefc5c63701292837d8be38d13
Emir Mekić <emir.mekic@live.com>
2019-12-23 17:20:55 +0100

    telescope added

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <a class="btn btn-primary" href="/items/new">New item</a>
+        <a class="btn btn-primary ml-2" href="/dashboard/edit">Edit items</a>
+    </div>
+</div>
+@endsection
