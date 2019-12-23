cd00ccde1cdd4574d4d394d7ac1e2a1004827a40
Emir Mekić <emir.mekic@live.com>
2019-12-23 17:20:26 +0100

    image field added to items db

--- database/migrations/2019_12_22_125949_create_items_table.php
+++ database/migrations/2019_12_22_125949_create_items_table.php
@@ -17,6 +17,7 @@ class CreateItemsTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->text('description');
+            $table->text('image');
             $table->float('price');
             $table->bigInteger('stock');
             $table->timestamps();
