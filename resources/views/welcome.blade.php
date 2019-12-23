11448fc5c63c34f8c71a0d046294262a1da9232d
Emir Mekić <emir.mekic@live.com>
2019-12-23 23:53:15 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <form action="/" class="form-inline row" method="GET">
+<form action="/" class="form-inline row" method="GET" style="margin-bottom:1em">
         @csrf
         <div class="form-group" style="margin:0 auto;">
             <input type="text" class="form-control" id="search" name="search" value="{{$searchVal}}" placeholder="Search...">
@@ -9,14 +8,16 @@
             <button class="btn btn-secondary ml-2"><a href="/">Clear</a></button>
         </div>
     </form>
+<div class="container">
     <div class="row">
         @foreach($items as $item)
-        <div class="card col-4 mt-2">
+        <div class="card col-4 mt-2 align-items-stretch">
+        <img class="card-img-top card-height pt-2" src="{{asset( $item -> image)}}" alt="Item image">
             <div class="card-body">
                 <h3 class="card-title">{{ $item -> name}}</h3>
                 <p class="card-text">{{ $item -> description}}</p>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Price: {{ $item -> price}}</li>
+                    <li class="list-group-item">Price: {{ $item -> price}} KM</li>
                     <li class="list-group-item">Stock: {{ $item->stock}}</li>
                 </ul>
             </div>
@@ -28,6 +29,8 @@
         </div>
         @endif
     </div>
+ 
+    
     <div class="container">
         <div class="pagination">
             {{ $items->links()}}
