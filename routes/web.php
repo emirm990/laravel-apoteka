6ea4cc042fd95af9a25f06b4c1d4bbd5aefc2569
Emir Mekić <emir.mekic@live.com>
2019-12-23 17:22:16 +0100

    routes for admin edit added

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,15 @@
 |
 */
 
-Route::get('/','ItemsController@index');
+Route::get('/', 'ItemsController@index');
 Route::post('/items/search', 'ItemsController@search');
 Route::get('/items/new', 'ItemsController@create')->middleware('isAdmin');
 Route::post('/items/new', 'ItemsController@store')->middleware('isAdmin');
-Route::get('/items/{item_id}/edit' , 'ItemsController@edit')->middleware('isAdmin');
-Route::get('/items/{item_id}' , 'ItemsController@show')->middleware('isAdmin');
+Route::get('/items/{item_id}/edit', 'ItemsController@edit')->middleware('isAdmin');
+Route::get('/items/{item_id}', 'ItemsController@show')->middleware('isAdmin');
 Route::patch('/items/{item_id}/edit', 'ItemsController@update')->middleware('isAdmin');
-
+Route::get('/dashboard/edit', 'AdminController@index')->middleware('isAdmin');
+Route::get('/dashboard', 'AdminController@home')->name('dashboard')->middleware('isAdmin');
+Route::post('dashboard/{item_id}/delete', 'AdminController@destroy')->middleware('isAdmin');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
