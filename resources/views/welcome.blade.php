d4fed49d69d5b65d26e2ec79c7d4b9c6b7c803a4
Emir Mekić <emir.mekic@live.com>
2019-12-23 17:21:54 +0100

    removed edit from home page listings

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,38 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <form action="/" class="form-inline row" method="GET">
-            @csrf
-            <div class="form-group" style="margin:0 auto;">
-                <input type="text" class="form-control" id="search" name="search" value="{{$searchVal}}" placeholder="Search...">
-                <button type="submit" class="btn btn-primary ml-2">Search</button>
-            </div>
-        </form>
-        </form>
-        
-        <div class="row">
+<div class="container">
+    <form action="/" class="form-inline row" method="GET">
+        @csrf
+        <div class="form-group" style="margin:0 auto;">
+            <input type="text" class="form-control" id="search" name="search" value="{{$searchVal}}" placeholder="Search...">
+            <button type="submit" class="btn btn-primary ml-2">Search</button>
+            <button class="btn btn-secondary ml-2"><a href="/">Clear</a></button>
+        </div>
+    </form>
+    <div class="row">
         @foreach($items as $item)
-            <div class="card col-4 mt-2">
-                <div class="card-body">
-                    <h3 class="card-title">{{ $item -> name}}</h3>
-                    <p class="card-text">{{ $item -> description}}</p>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Price: {{ $item -> price}}</li>
-                        <li class="list-group-item">Stock: {{ $item->stock}}</li>
-                        @if(isset($user->admin))
-                            @if($user->admin === 1)
-                                <li class="list-group-item">
-                                    <a class="card-link" href="/items/{{ $item->id}}/edit">Edit</a>
-                                </li>
-                            @endif
-                        @endif
-                    </ul>
-                </div>
+        <div class="card col-4 mt-2">
+            <div class="card-body">
+                <h3 class="card-title">{{ $item -> name}}</h3>
+                <p class="card-text">{{ $item -> description}}</p>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Price: {{ $item -> price}}</li>
+                    <li class="list-group-item">Stock: {{ $item->stock}}</li>
+                </ul>
             </div>
+        </div>
         @endforeach
         @if(count($items)===0)
-            <div>
-                <p> Sorry, not items match your search.</p>
-            </div>
+        <div>
+            <p> Sorry, not items match your search.</p>
+        </div>
         @endif
     </div>
     <div class="container">
         <div class="pagination">
-        {{ $items->links()}}
+            {{ $items->links()}}
         </div>
-        
+
     </div>
-    
-@endsection
+
+    @endsection
