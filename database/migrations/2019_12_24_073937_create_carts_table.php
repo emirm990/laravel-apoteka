f8f4dfbaa9f8678db4c0db9f928d31fd0a64af1b
Emir Mekić <emir.mekic@live.com>
2019-12-24 17:16:56 +0100

    Create 2019_12_24_073937_create_carts_table.php

---
+++ database/migrations/2019_12_24_073937_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->bigInteger('item_id');
+            $table->bigInteger('count');
+            $table->float('total');
+            $table->boolean('processed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
