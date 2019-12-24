6d5a1984801510489cb559a9d506ca296711bb6e
Emir Mekić <emir.mekic@live.com>
2019-12-24 17:17:34 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,27 @@
 @extends('layouts.app')
 @section('content')
 <form action="/" class="form-inline row" method="GET" style="margin-bottom:1em">
-        @csrf
-        <div class="form-group" style="margin:0 auto;">
-            <input type="text" class="form-control" id="search" name="search" value="{{$searchVal}}" placeholder="Search...">
-            <button type="submit" class="btn btn-primary ml-2">Search</button>
-            <button class="btn btn-secondary ml-2"><a href="/">Clear</a></button>
-        </div>
-    </form>
+    @csrf
+    <div class="form-group" style="margin:0 auto;">
+        <input type="text" class="form-control" id="search" name="search" value="{{$searchVal}}" placeholder="Search...">
+        <button type="submit" class="btn btn-primary ml-2">Search</button>
+        <button class="btn btn-secondary ml-2"><a href="/">Clear</a></button>
+    </div>
+</form>
 <div class="container">
     <div class="row">
         @foreach($items as $item)
         <div class="card col-4 mt-2 align-items-stretch">
-        <img class="card-img-top card-height pt-2" src="{{asset( $item -> image)}}" alt="Item image">
+            <img class="card-img-top card-height pt-2" src="{{asset( $item -> image)}}" alt="Item image">
             <div class="card-body">
                 <h3 class="card-title">{{ $item -> name}}</h3>
                 <p class="card-text">{{ $item -> description}}</p>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">Price: {{ $item -> price}} KM</li>
                     <li class="list-group-item">Stock: {{ $item->stock}}</li>
+                    @auth
+                    <button onClick="addToCart('{{$item->id}}','{{auth()->id()}}')" class="btn btn-primary">Add to cart</button>
+                    @endauth
                 </ul>
             </div>
         </div>
@@ -29,8 +32,8 @@
         </div>
         @endif
     </div>
- 
-    
+
+
     <div class="container">
         <div class="pagination">
             {{ $items->links()}}
@@ -39,3 +42,17 @@
     </div>
 
     @endsection
+    <script>
+        function addToCart(item_id, user_id) {
+            console.log(item_id, user_id);
+            axios.post('/cart/add', {
+                    item_id,
+                    user_id
+                })
+                .then(res => {
+                    console.log(res);
+                }).catch(err => {
+                    console.log(err)
+                })
+        }
+    </script>
