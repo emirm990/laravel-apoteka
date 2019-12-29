57d9f3579836f8178fb3f985651f63abd00b9fb8
Emir Mekić <emir.mekic@live.com>
2019-12-29 01:11:12 +0100

    Update ItemsSeeder.php

--- database/seeds/ItemsSeeder.php
+++ database/seeds/ItemsSeeder.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Storage;
 
 class ItemsSeeder extends Seeder
 {
+   
     /**
      * Run the database seeds.
      *
@@ -11,6 +14,19 @@ class ItemsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $path = storage_path('app\public\images');
+        $faker = Faker::create();
+        for($i=1;$i<=100;$i++){
+            DB::table('items')->insert([
+                'name' => $faker->lastName,
+                'description' => $faker->realText($maxNbChars = 200, $indexSize = 2),
+                'image' => "/storage/images/" . $faker->image($path, 800, 600, 'animals', false),
+                'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 1, $max = 25) ,
+                'stock' => $faker->numberBetween($min = 10, $max = 2500),
+                'created_at' => $faker->dateTimeThisYear($max = 'now', $timezone = null),
+                'updated_at' => $faker->dateTimeThisYear($max = 'now', $timezone = null)
+            ]);
+        }
+        
     }
 }
