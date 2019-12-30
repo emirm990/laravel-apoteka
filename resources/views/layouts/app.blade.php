97146a1d366fa37d4239219b69b5852eee77ccce
Emir Mekić <emir.mekic@live.com>
2019-12-30 16:26:03 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
@@ -31,7 +31,7 @@
                 <div>
                     <a href="{{ route('cart') }}">
                         <div>
-                            <p>Cart</p>
+                        <i class="fa fa-shopping-cart"></i>
                             <span id="items_in_cart">0</span>
                         </div>
                     </a>
