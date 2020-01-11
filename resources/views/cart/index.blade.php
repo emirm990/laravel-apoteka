94f90c6dafd1e9fa66eb69efba8f183fff0afe82
Emir Mekić <emirm@smartlab.ba>
2020-01-11 12:04:46 +0100

    Update index.blade.php

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row cart-all">
         <h1 class="cart-heading">Cart</h1>
         <div class="cart-container">
-            <items-container :items='{{ json_encode($cartItems) }}' />
+            <items-container :items='{{ json_encode($cartItems) }}' :cart="{{ json_encode(true) }}" />
         </div>
     </div>
 </div>
